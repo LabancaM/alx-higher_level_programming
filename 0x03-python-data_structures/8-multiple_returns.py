0c546ca8d1988d4540f721686f7f666e1a6cef48
LabancaM <marc.ayi@imsp-uac.org>
2024-01-08 17:55:56 -0800

    python data structure

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,4 +3,7 @@ def multiple_returns(sentence):
     length = len(sentence)
 
     if length > 0:
-        return length, sentence[0]
+        first = sentence[0]
+    else:
+        first = None
+    return length, first
