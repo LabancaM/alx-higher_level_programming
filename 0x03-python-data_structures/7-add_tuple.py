6efa1fa6b7763d80056beea14bd3d8c714f0ae03
LabancaM <marc.ayi@imsp-uac.org>
2024-01-08 18:14:10 -0800

    python data structure

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,7 +2,23 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     n = len(tuple_a)
     m = len(tuple_b)
-    if n < 2 or m < 2:
-        if
-        
-
+    if n == 0:
+        a, b = 0, 0
+        if m == 0:
+            c, d = 0, 0
+        if m == 1:
+            c, d = tuple_b[0], 0
+        if m >=2:
+            c, d = tuple_b[0], tuple_b[1]
+    elif m == 0:
+        c, d = 0, 0
+        if n == 0:
+            a, b = 0, 0
+        if n == 1:
+            a, b = tuple_a[0], 0
+        if m >=2:
+            a, b = tuple_a[0], tuple_a[1]
+    elif m >=2 and n >=2:
+        a, b = tuple_a[0], tuple_a[1]
+        c, d = tuple_b[0], tuple_b[1]
+    return a+c, b+d
