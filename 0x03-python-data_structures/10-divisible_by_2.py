8bdfafb0c7c7e001bafb3f006bfff75d52ef5fd6
LabancaM <marc.ayi@imsp-uac.org>
2024-01-09 03:01:02 -0800

    python data structure

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     if len(my_list) > 0:
-        return [ True if i % 2 == 0 else False for i in my_list]
+        return [True if i % 2 == 0 else False for i in my_list]
     else:
         return my_list
