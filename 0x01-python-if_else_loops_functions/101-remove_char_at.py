b59be984608246d6fc71fffaf335018ece565b4b
LabancaM <marc.ayi@imsp-uac.org>
2024-05-19 21:31:49 -0700

    python heloo

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    length = len(str)
+    
+    if n < 0 or n > length - 1:
+        return str
+    else:
+        stri = ""
+        for i in range(length):
+            if i == n:
+                continue
+            stri = stri + str[i]
+        return stri
