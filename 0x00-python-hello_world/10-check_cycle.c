275802c5da3f2be1788c49157638292afc64cb1d
LabancaM <marc.ayi@imsp-uac.org>
2024-05-19 17:32:14 -0700

    I am back

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * check_cycle - chech if single linked list is cycle
+ *
+ * @list: input is list
+ * Return: 0 if is cycle 1 otherwise
+ */
+int check_cycle(listint_t *list)
+{
+}
---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * is_palindrome - check if list is palindrome
+ *
+ * @head: list in parameter
+ * Return: 1 if list is palindrome, 0 otherwise
+ */
+int is_palindrome(listint_t **head)
+{
+
+}
