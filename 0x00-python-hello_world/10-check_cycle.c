cf9c5a0a1b82fb9222294a7127b973105e068412
LabancaM <marc.ayi@imsp-uac.org>
2024-05-19 18:09:11 -0700

    python heloo

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -9,4 +9,22 @@
  */
 int check_cycle(listint_t *list)
 {
+	listint_t *turtle, *hare;
+
+	if (list == NULL || list->next == NULL)
+		return (0);
+
+	turtle = list->next;
+	hare = list->next->next;
+
+	while (turtle && hare && hare->next)
+	{
+		if (turtle == hare)
+			return (1);
+
+		turtle = turtle->next;
+		hare = hare->next->next;
+	}
+
+	return (0);
 }
