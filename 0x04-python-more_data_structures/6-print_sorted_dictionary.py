ebe7cc88b90c75d5b79cfdd865cb22a901a12fc9
LabancaM <marc.ayi@imsp-uac.org>
2024-05-22 19:55:57 -0700

    more data structure in python

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
     new_dict = dict(sorted(a_dictionary.items()))
-    for i,j in zip(new_dict.keys(), new_dict.values()):
+    for i, j in zip(new_dict.keys(), new_dict.values()):
         print("{}: {}".format(i, j))
     return new_dict
