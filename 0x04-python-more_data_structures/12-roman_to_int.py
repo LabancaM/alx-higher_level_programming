8708cf06566bb400fc0058f3994595befc6e2226
LabancaM <marc.ayi@imsp-uac.org>
2024-05-27 08:10:13 -0700

    more data structure in python

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,7 @@
 def roman_to_int(roman_string):
     dicte = {"I": 1, "V": 5, "X": 10, "D": 500, "L": 50, "C": 100}
     sum = 0
-    if roman_string is None or roman_string is not str:
+    if roman_string is None or type(roman_string) is not str:
         return 0
     length = len(roman_string)
     for i in range(length - 1):
