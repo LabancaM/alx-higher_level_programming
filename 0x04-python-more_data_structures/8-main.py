87d19af91009b443ed637eb11295037780953f71
LabancaM <marc.ayi@imsp-uac.org>
2024-05-27 06:34:15 -0700

    more data structure in python

--- 0x04-python-more_data_structures/8-main.py
+++ 0x04-python-more_data_structures/8-main.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
-number_keys = __import__('5-number_keys').number_keys
+simple_delete = __import__('8-simple_delete').simple_delete
+print_sorted_dictionary = \
+    __import__('6-print_sorted_dictionary').print_sorted_dictionary
 
-a_dictionary = { 'language': "C", 'number': 13, 'track': "Low level" }
-nb_keys = number_keys(a_dictionary)
-print("Number of keys: {:d}".format(nb_keys))
+a_dictionary = { 'language': "C", 'Number': 89, 'track': "Low", 'ids': [1, 2, 3] }
+new_dict = simple_delete(a_dictionary, 'track')
+print_sorted_dictionary(a_dictionary)
+print("--")
+print_sorted_dictionary(new_dict)
+
+print("--")
+print("--")
+new_dict = simple_delete(a_dictionary, 'c_is_fun')
+print_sorted_dictionary(a_dictionary)
+print("--")
+print_sorted_dictionary(new_dict)
