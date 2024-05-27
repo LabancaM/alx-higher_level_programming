e10d456662139a06a6c7de5a06448db9a89b4dae
LabancaM <marc.ayi@imsp-uac.org>
2024-05-27 08:02:45 -0700

    more data structure in python

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    dicte = {"I": 1, "V": 5, "X": 10, "D": 500, "L": 50, "C": 100}
+    sum = 0
+    length = len(roman_string)
+    for i in range(length - 1):
+        if roman_string[i] == "I" and \
+                (roman_string[i + 1] == "V" or roman_string[i + 1] == "X"):
+            sum -= 1
+            continue
+        sum += dicte[roman_string[i]]
+    sum += dicte[roman_string[-1]]
+    return sum
