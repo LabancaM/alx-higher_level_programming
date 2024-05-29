e0fd55f6ade687fd631ef9e2efb0ed8a30b78880
LabancaM <marc.ayi@imsp-uac.org>
2024-05-29 15:54:40 -0700

    Exception in python

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    try:
-        for i in my_list[:x]:
-            print(i, end="")
-        print()
-        return my_list[x - 1]
-    except IndexError:
-        if  x == 0:
-            return 0
-        else:
+    if x == 0:
+        return 0
+    else:
+        try:
+            for i in my_list[:x]:
+                print(i, end="")
+            print()
+            return my_list[x - 1]
+        except IndexError:
             return my_list[-1]
--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    try:
-        for i in my_list[:x]:
+    for i in my_list[:x]:
+        try:
             val = int(i)
             print("{:d}".format(val), end="")
-        print()
-        return my_list[x - 1]
-    except (IndexError, ValueError, TypeError):
-        x += 1
+        except (IndexError, ValueError, TypeError):
+            x += 1
