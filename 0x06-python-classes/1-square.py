07cccb7a61982efe07b23a61f9d5bc4c673bd802
LabancaM <marc.ayi@imsp-uac.org>
2024-05-30 04:19:57 -0700

    Classes and Objects

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+""" This is class Square."""
+
+
+class Square:
+    """ This is class Square."""
+
+    def __init__(self, size):
+        """Initialisation function.
+
+        args
+            size: the size of square.
+        """
+        self.__size = size
