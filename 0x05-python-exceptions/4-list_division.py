94fa30e006249e759065d11a0d9b2f8bf0d81703
LabancaM <marc.ayi@imsp-uac.org>
2024-05-29 17:30:49 -0700

    Exception in python

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    liste = []
+    for index in range(list_length):
+        try :
+            res = my_list_1[index] / my_list_2[index]
+        except ZeroDivisionError:
+            res = 0
+            print("division by 0")
+        except TypeError:
+            res = 0
+            print("wrong type")
+        except IndexError:
+            res = 0
+            print("out of riange")
+        finally:
+            liste.append(res)
+    return liste
