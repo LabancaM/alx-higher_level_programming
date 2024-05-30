5473c6fc9ea744c0c5eefa531b0bf5c4b891ee68
LabancaM <marc.ayi@imsp-uac.org>
2024-05-30 04:43:36 -0700

    Classes and Objects

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+""" This is class Square."""
+
+
+class Square:
+    """ This is class Square."""
+
+    def __init__(self, size=0):
+        """Initialisation function.
+
+        args
+            size: the size of square.
+        """
+        if isinstance(size, int) is False:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """ compute the area of square."""
+        return self.__size * self.__size
