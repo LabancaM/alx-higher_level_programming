f8a3c71518dcbaffe09c715a54976532b883f5de
LabancaM <marc.ayi@imsp-uac.org>
2024-06-04 07:13:58 -0700

    Python - Input/Output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+""" read file"""
+
+
 def read_file(filename=""):
+    """ function to read file
+
+        Args
+            filename: text file name
+    """
     with open(filename, encoding='utf-8') as f:
         print(f.read(), end="")
